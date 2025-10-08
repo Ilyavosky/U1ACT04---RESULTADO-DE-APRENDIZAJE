b26ddae8d31d27e6871b1b0d2049b4e556208d14
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-07 19:48:22 -0600

    feat: Adición del método findFirst

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -15,4 +15,18 @@ public class ArrayUtils {
         }
         return resultado.toArray(Arrays.copyOf(array, 0));
     }
+
+    public static <T> T findFirst(T[] array, Predicate<T> predicate) {
+        if (array == null || array.length == 0) {
+            return null;
+        }
+        for(T elemento :array){
+            if (predicate.test(elemento)) {
+                return elemento;
+            }
+        }
+        return null;
+    }
+
+
 }
