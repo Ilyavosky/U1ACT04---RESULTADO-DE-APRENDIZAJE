4b322980110627b651660fbd0fb5e702ae5bec9f
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-07 19:30:37 -0600

    feat: Mejora en la lógica

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -2,15 +2,16 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class ArrayUtils {
-    public static <T> T[] filter(T[] array, Predicate<T> predicate){
-        filter(array, predicate);
-        if (array.length == 0 || array[0] == null){
+    public static <T> T[] filter(T[] array, Predicate<T> predicate) {
+        if (array == null || array.length == 0) {
             return array;
-        }else {
-            ArrayList<T> aprovo = new ArrayList<>();
-            aprovo.add(array[0]);
         }
-
-        return filter(array,  predicate);
+        ArrayList<T> resultado = new ArrayList<>();
+        for(T elemento :array){
+            if (predicate.test(elemento) == true) {
+                resultado.add(elemento);
+            }
+        }
+        return resultado.toArray((T[]) new Object[resultado.size()]);
     }
 }
