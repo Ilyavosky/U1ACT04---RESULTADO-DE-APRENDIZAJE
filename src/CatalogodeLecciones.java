3ab3248fbdce67684c4a10e0ce43bbbad28737e2
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-07 18:39:39 -0600

    Primer Commit

---
+++ src/CatalogodeLecciones.java
@@ -0,0 +1,16 @@
+public class CatalogodeLecciones <T> {
+    private String titulo;
+    private String tema;
+    private String nivel;
+
+    public CatalogodeLecciones() {
+    }
+
+    public CatalogodeLecciones(String titulo, String tema, String nivel) {
+        this.titulo = titulo;
+        this.tema = tema;
+        this.nivel = nivel;
+    }
+
+
+}
