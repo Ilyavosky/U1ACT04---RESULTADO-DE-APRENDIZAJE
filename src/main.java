eb5604c6f7b4958e268ed600deb26cd9b2a1bb4f
Ilyavosky <ilyacortesruiz@gmail.com>
2025-10-08 18:17:22 -0600

    feat: Display del cátalogo de lecciones

--- src/main.java
+++ src/main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
+        Leccion[] catalogo = new Leccion[]{
+                new Leccion("Circulo de DO", "Teoría", "Principiante"),
+                new Leccion("Circulo de RE", "Teoría", "Principiante"),
+                new Leccion("Circulo de MI", "Teoría", "Principiante"),
+                new Leccion("Circulo de FA", "Teoría", "Principiante"),
+                new Leccion("Circulo de SOL", "Teoría", "Principiante")
+        };
+
+        System.out.println("Catalogo de Lecciones disponibles");
+        System.out.println(Arrays.toString(catalogo));
 
     }
 }
